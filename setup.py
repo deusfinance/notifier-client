550ac503977b01484220d666675b89d6941850fe
ali.ghorbani <ali.ghorbani@mehrparsict.com>
2022-05-09 15:42:50 +0430

    the files for pip deployment added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telegram_notifier_client',
+    version='0.1',
+    author="Ali Ghorbani",
+    author_email='alighorbani29@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='telegram notifier client',
+    url='https://github.com/deusfinance/notifier-client.git',
+    install_requires=[
+    ],
+
+)
