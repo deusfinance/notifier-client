3815c4b44850cc905df65e309764035b5c33bb10
ali.ghorbani <alighorbani29@gmail.com>
2023-11-14 18:19:18 +0330

    version added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='telegram_notifier_client',
-    version='1',
+    version='1.0.1',
     author="Ali Ghorbani",
     author_email='alighorbani29@gmail.com',
     packages=find_packages('src'),
